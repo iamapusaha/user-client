25a20538bc90accfee0f18a06bcbdc881a02cd8b
iamapusaha <apusaha392@gmail.com>
2023-10-11 22:17:06 +0600

    send data to the server from form input

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,8 +1,13 @@
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 
 const Users = () => {
-    const users = useLoaderData();
+    const [users, setUsers] = useState([])
+    const data = useLoaderData();
+    useEffect(() => {
+        setUsers(data)
+    }, [data])
     const handleAddUser = e => {
         e.preventDefault()
         const form = e.target;
@@ -10,6 +15,18 @@ const Users = () => {
         const email = form.email.value;
         const user = { name, email }
         console.log(user);
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('inside post response', data);
+            })
+
     }
     return (
         <div>
@@ -19,6 +36,9 @@ const Users = () => {
                 <input type="submit" value="Add User" />
             </form>
             <h1>Total no of user {users.length}</h1>
+            {
+                users.map(user => <li key={user.id}>{user.id} : {user.email}</li>)
+            }
         </div>
     );
 };
